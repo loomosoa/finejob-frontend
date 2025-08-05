edf014b80243f2e3c7792999bf4dd73b0619889c
Your Name <you@example.com>
2025-08-05 19:56:22 +0300

    Making

--- src/components/JobSeek.tsx
+++ src/components/JobSeek.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import vacResFields2 from "../assets/imgs/vac_res_fields.png";
+import vacResArrow from "../assets/imgs/vac_res_arrow.svg";
 
 const JobSeek: React.FC = () => {
   return (
@@ -13,7 +14,35 @@ const JobSeek: React.FC = () => {
       <div className="jobseek-wrapper">
         <div className="container">
           <div className="dash-frame">
-            <div className="dash-frame-inner"></div>
+            <div className="dash-frame-inner">
+              <div className="dash-field">
+                <div className="title-wrapper">
+                  <div className="title">Создать</div>
+                </div>
+                <div id="selectVacRes" className="field">
+                  Вакансию | Резюме{" "}
+                  <img className="vac-res-arrow" src={vacResArrow} />
+                </div>
+              </div>
+              <div className="dash-field">
+                <div className="title-wrapper">
+                  <div className="title">Заголовок</div>
+                </div>
+                <div className="field-wrapper">
+                  <input className="field" name="title" />
+                  <div className="under-label">140 символов</div>
+                </div>
+              </div>
+              <div id="vas-res-description" className="dash-field">
+                <div className="title-wrapper">
+                  <div className="title">Описание</div>
+                </div>
+                <div className="field-wrapper">
+                  <textarea className="field" name="title" />
+                  <div className="under-label">280 символов</div>
+                </div>
+              </div>
+            </div>
           </div>
           JobSeek
         </div>
