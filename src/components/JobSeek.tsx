5ce514bd27d59fcc2ddef370131de60b5243d296
Your Name <you@example.com>
2025-08-06 20:20:36 +0300

    Make skills container

--- src/components/JobSeek.tsx
+++ src/components/JobSeek.tsx
@@ -2,26 +2,69 @@ import React from "react";
 
 // import vacResFields2 from "../assets/imgs/vac_res_fields.png";
 import vacResArrow from "../assets/imgs/vac_res_arrow.svg";
+import selectImg from "../assets/imgs/select_img.svg";
 import axios from "axios";
 
-type TSkills = {};
-
 const JobSeek: React.FC = () => {
-  const [skills, setSkills] = React.useState([]);
+  const [langs, setLangs] = React.useState<any[]>([]);
+  const [techs, setTechs] = React.useState<any[]>([]);
+  const [frameworks, setFrameworks] = React.useState<any[]>([]);
+
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     const getSkills = async () => {
-      const { data } = await axios.get(
-        `http://finejob-api.local/api/v1/skills`
-      );
+      try {
+        const { data } = await axios.get(
+          "http://finejob-api.local/api/v1/skills"
+        );
+        const languagesObject = data.data?.[0]?.languages;
 
-      setSkills(data.data);
+        if (typeof languagesObject === "object" && languagesObject !== null) {
+          const flattenedLanguages = Object.values(languagesObject).flat();
+          setLangs(flattenedLanguages);
+        } else {
+          console.error(
+            "API response for languages is not an object:",
+            languagesObject
+          );
+          setError("Ошибка формата данных от сервера.");
+        }
 
-      // console.log(data.data[0].languages);
+        const frameworksObject = data.data?.[2]?.frameworks;
+        if (typeof frameworksObject === "object" && frameworksObject !== null) {
+          const flattenedFW = Object.values(frameworksObject).flat();
+          setFrameworks(flattenedFW);
+        } else {
+          console.error(
+            "API response for frameworksObject is not an object:",
+            frameworksObject
+          );
+          setError("Ошибка формата данных от сервера.");
+        }
+
+        const techsObject = data.data?.[1]?.technologies;
+
+        if (typeof techsObject === "object" && techsObject !== null) {
+          const flattenedTechs = Object.values(techsObject).flat();
+          setTechs(flattenedTechs);
+        } else {
+          console.error(
+            "API response for techs is not an object:",
+            techsObject
+          );
+          setError("Ошибка формата данных от сервера.");
+        }
+      } catch (err) {
+        console.error("Error fetching skills:", err);
+        setError("Не удалось загрузить навыки. Пожалуйста, попробуйте позже.");
+      } finally {
+        setLoading(false);
+      }
     };
 
     getSkills();
-    console.log(skills);
   }, []);
 
   return (
@@ -73,61 +116,58 @@ const JobSeek: React.FC = () => {
                   <span className="search">Поиск</span>
                 </div>
                 <div className="skills-grid">
-                  {/* {languages.map((lang, i) => (
-                        <li
-                          key={i}
-                          onClick={() => changeLanguage(lang)}
-                          className={i18n.language === lang ? "active" : ""}
-                        >
-                          {t(lang)}
-                        </li>
-                      ))} */}
-                  {/* {skills?[0].map((lang, i) => ( 
-                    <div>{lang}</div>
-                  ))} */}
-                  {/* {skills?.languages.map((lang, i) => (
-                    <li key={i}>{lang}</li>
-                  ))} */}
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">PHP</div>
-                    <div className="grade">Any</div>
-                  </div>
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">JavaScript</div>
-                    <div className="grade">Any</div>
-                  </div>
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">Rust</div>
-                    <div className="grade">Any</div>
-                  </div>
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">Python</div>
-                    <div className="grade">Any</div>
-                  </div>
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">TypeScript</div>
-                    <div className="grade">Any</div>
-                  </div>
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">SQL</div>
-                    <div className="grade">Any</div>
-                  </div>
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">Java</div>
-                    <div className="grade">Any</div>
-                  </div>
-                  <div className="item">
-                    <div className="checkbox"></div>
-                    <div className="element">C#</div>
-                    <div className="grade">Any</div>
-                  </div>
+                  {loading && <p>Загрузка...</p>}
+                  {error && <p>{error}</p>}
+                  {langs &&
+                    langs.map((lang: string) => (
+                      <div className="item" key={lang}>
+                        <div className="checkbox">
+                          <img src={selectImg} />
+                        </div>
+                        <div className="element">{lang}</div>
+                        <div className="grade">Any</div>
+                      </div>
+                    ))}
+                </div>
+              </div>
+              <div className="skills-container">
+                <div className="title">
+                  <span className="skills-type">Languages</span>
+                  <span className="search">Поиск</span>
+                </div>
+                <div className="skills-grid">
+                  {loading && <p>Загрузка...</p>}
+                  {error && <p>{error}</p>}
+                  {techs &&
+                    techs.map((tech: string) => (
+                      <div className="item" key={tech}>
+                        <div className="checkbox">
+                          <img src={selectImg} />
+                        </div>
+                        <div className="element">{tech}</div>
+                        <div className="grade">Any</div>
+                      </div>
+                    ))}
+                </div>
+              </div>
+              <div className="skills-container">
+                <div className="title">
+                  <span className="skills-type">Languages</span>
+                  <span className="search">Поиск</span>
+                </div>
+                <div className="skills-grid">
+                  {loading && <p>Загрузка...</p>}
+                  {error && <p>{error}</p>}
+                  {frameworks &&
+                    frameworks.map((framework: string) => (
+                      <div className="item" key={framework}>
+                        <div className="checkbox">
+                          <img src={selectImg} />
+                        </div>
+                        <div className="element">{framework}</div>
+                        <div className="grade">Any</div>
+                      </div>
+                    ))}
                 </div>
               </div>
             </div>
